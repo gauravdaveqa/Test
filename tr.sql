fcf529a872e8a0989d4f96f6a22b68edcb882553
gaurav dave <garvdave50@gmail.com>
2020-09-26 19:05:58 +0530

    message

---
+++ tr.sql
@@ -0,0 +1,9 @@
+ use dots1;
+
+create table dots1(
+   tutorial_id INT NOT NULL AUTO_INCREMENT,
+   tutorial_title VARCHAR(100) NOT NULL,
+   tutorial_author VARCHAR(40) NOT NULL,
+   submission_date DATE,
+   PRIMARY KEY ( tutorial_id )
+);
