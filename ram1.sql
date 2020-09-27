e678f1c108768ff5774044ca5745bee383d3d9f7
gaurav dave <garvdave50@gmail.com>
2020-09-27 11:21:03 +0530

    dave

---
+++ ram1.sql
@@ -0,0 +1,5 @@
+create table ram1 (Empid int primary key,FirstName varchar(50),LastName varchar(50),salary float,MobileNo varchar(15) )
+
+select * from ram1
+
+insert into ram1(Empid,FirstName,LastName,salary,MobileNo)VALUES(3,'Nishant','Dave',15000,9829033983);
