b96f6b2cf911bd44f80d837d0a1ab8ba4e750eb8
gauravdaveqa <56600561+gauravdaveqa@users.noreply.github.com>
2020-09-27 11:02:48 +0530

    Delete ram1.sql

--- ram1.sql
+++
@@ -1,5 +0,0 @@
-create table ram1 (Empid int primary key,FirstName varchar(50),LastName varchar(50),salary float,MobileNo varchar(15) )
-
-select * from ram1
-
-insert into ram1(Empid,FirstName,LastName,salary,MobileNo)VALUES(3,'Nishant','Dave',15000,9829033983);
